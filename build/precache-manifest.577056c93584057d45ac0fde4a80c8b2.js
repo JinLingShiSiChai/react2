f5fcb0ab05d914d77ef6b32d7111696d4ddbd9dd
JinLingShiSiChai <2259094155@qq.com>
2020-10-18 15:51:37 +0800

    demo

---
+++ build/precache-manifest.577056c93584057d45ac0fde4a80c8b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7711128ea71d962893ac9304d010377b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdace70b8b0bc49cdc11",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "a7a71709b6447952d273",
+    "url": "/static/css/main.a3a8b971.chunk.css"
+  },
+  {
+    "revision": "bdace70b8b0bc49cdc11",
+    "url": "/static/js/2.63b1a052.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.63b1a052.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7a71709b6447952d273",
+    "url": "/static/js/main.ecb6b1f3.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
